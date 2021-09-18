65f237e9fad44ff8cff35e53074bd3a35e49af46
Tanner Fleming <tannerfleming@csus.edu>
2021-09-18 23:32:22 +0000

    We a dded the buttons and the style sheet

---
+++ src/main/resources/templates/script.js
@@ -0,0 +1,7 @@
+
+      function goodFeeling() {
+        document.getElementById("how-are-you-feeling-response").innerHTML = "Yay";
+      }
+      function badFeeling() {
+        document.getElementById("how-are-you-feeling-response").innerHTML = "I'm sorry";
+      }
