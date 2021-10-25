ba5f518c2adfc134ed6ecc18b49bbdf370a09ae5
Katrina Etchison <kketchison@csus.edu>
2021-10-25 03:25:54 +0000

    refactored login and registration pages to react and changed styling to be...

--- src/main/resources/public/script.js
+++
@@ -1,6 +0,0 @@
-function openForm() {
-  document.getElementById('survey-activities-popup').style.display = 'block';
-}
-function closeForm() {
-  document.getElementById('survey-activities-popup').style.display = 'none';
-}
