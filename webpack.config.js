881809c731f5131f4f53cab7b57c1449587847a6
Tanner Fleming <tannerfleming@csus.edu>
2021-11-07 05:32:36 +0000

    Integrated bootstrap and sass, refactored pages with bootstrap, removed...

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,11 @@ module.exports = {
         }],
       },
       {
-        test: /\.css$/,
+        test: /\.(css|scss)$/,
         use: [
           'style-loader',
           'css-loader',
+          'sass-loader',
         ],
       },
       {
